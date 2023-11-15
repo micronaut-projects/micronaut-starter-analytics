eb9decaa951a795560f5187d73fc95fe679d6bb8
Tim Yates <tim.yates@gmail.com>
2023-11-15 16:37:00 +0000

    Add Develocity plugin and config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,21 @@
+import org.gradle.internal.impldep.org.eclipse.jgit.lib.ObjectChecker.tag
+
+plugins {
+    id("com.gradle.enterprise") version("3.15")
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.11.3"
+}
+
+gradleEnterprise {
+    server = "https://ge.micronaut.io"
+    buildScan {
+        publishAlways()
+        this.javaClass.getMethod("publishIfAuthenticated").invoke(this)
+        val vendor = providers.systemProperty("java.vendor")
+        if (vendor.isPresent) {
+            tag("vendor:" + vendor.get().lowercase().replace(Regex("\\W+"), "_"))
+        }
+        tag("jdk:" + JavaVersion.current().majorVersion)
+    }
+}
+
 rootProject.name="starter-analytics"
