935857a3895443c224281d60b3a842fb245f30e0
Sergio del Amo <sergio.delamo@softamo.com>
2023-11-16 13:17:30 +0100

    more logging

--- src/main/java/io/micronaut/starter/analytics/postgres/security/ApiKeyRepository.java
+++ src/main/java/io/micronaut/starter/analytics/postgres/security/ApiKeyRepository.java
@@ -18,6 +18,8 @@ package io.micronaut.starter.analytics.postgres.security;
 import io.micronaut.core.annotation.NonNull;
 import jakarta.inject.Singleton;
 import jakarta.validation.constraints.NotBlank;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
@@ -30,11 +32,13 @@ import static java.util.stream.Collectors.toMap;
  */
 @Singleton
 class ApiKeyRepository {
+    private static final Logger LOG = LoggerFactory.getLogger(ApiKeyRepository.class);
 
     private final Map<String, String> keys;
 
     ApiKeyRepository(List<ApiKeyConfiguration> keys) {
         this.keys = keys.stream().collect(toMap(ApiKeyConfiguration::getKey, ApiKeyConfiguration::getName));
+        LOG.trace("keys #{}", keys.size());
     }
 
     /**
