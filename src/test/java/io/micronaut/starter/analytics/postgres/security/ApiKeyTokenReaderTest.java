45cba71b955007b785c1a51869a713e1cbc89e77
Sergio del Amo <sergio.delamo@softamo.com>
2023-11-16 14:54:58 +0100

    diable bearer token reader

--- src/test/java/io/micronaut/starter/analytics/postgres/security/ApiKeyTokenReaderTest.java
+++ src/test/java/io/micronaut/starter/analytics/postgres/security/ApiKeyTokenReaderTest.java
@@ -1,15 +1,12 @@
 package io.micronaut.starter.analytics.postgres.security;
 
 import io.micronaut.context.BeanContext;
-import io.micronaut.security.token.bearer.BearerTokenReader;
 import io.micronaut.security.token.reader.TokenReader;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,8 +20,7 @@ class ApiKeyTokenReaderTest {
     void apiKeyTokenReaderTakesPrecedence() {
         Collection<TokenReader> tokenReaders = beanContext.getBeansOfType(TokenReader.class);
         assertNotNull(tokenReaders);
-        List<TokenReader> tokenReaderList = new ArrayList<>(tokenReaders);
-        assertTrue(tokenReaderList.get(0) instanceof ApiKeyTokenReader);
-        assertTrue(tokenReaderList.get(1) instanceof BearerTokenReader);
+        assertEquals(1, tokenReaders.size());
+        assertTrue(tokenReaders.iterator().next() instanceof ApiKeyTokenReader);
     }
 }
