98e675e309461a5302d415f72e2915c9219b36bd
Sergio del Amo <sergio.delamo@softamo.com>
2023-12-19 13:34:31 +0100

    graal fixes

--- src/main/java/io/micronaut/starter/analytics/postgres/charts/PieChart.java
+++ src/main/java/io/micronaut/starter/analytics/postgres/charts/PieChart.java
@@ -2,10 +2,12 @@ package io.micronaut.starter.analytics.postgres.charts;
 
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.annotation.ReflectiveAccess;
 import io.micronaut.views.fields.messages.Message;
 
 import java.util.List;
 
+@ReflectiveAccess
 @Introspected
 public record PieChart(@NonNull String id,
                        @NonNull Message title,
